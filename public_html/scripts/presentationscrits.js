08fc76fe2c270783879d5aae1cfc9b9306f29cca
Paulo Pinheiro <Paulo Pinheiro@cedis.pt>
2016-06-05 17:49:48 +0100

    ok

---
+++ public_html/scripts/presentationscrits.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function OnMouseInFoto (elem) {
+    elem.style.border = "2px solid blue";
+}
+        
+function OnMouseOutFoto (elem) {
+    elem.style.border = "";
+}
